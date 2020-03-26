98adcc9accc91b1465842e16b3315245cf368df9
Jakob Braun <jakob.braun@posteo.de>
2020-03-26 14:13:51 +0100

    addded documentation ci

---
+++ .ci/setup-ssh.sh
@@ -0,0 +1,17 @@
+ #!/bin/bash
+# setup-ssh.sh: load the SSH key 
+
+set -ev
+declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
+# Decrypt the file containing the private key (put the real name of the variables)
+openssl aes-256-cbc \
+  -K $encrypted_4a328ad72b8a_key \
+  -iv $encrypted_4a328ad72b8a_iv \
+  -in ".ci/travis_deploy_key.enc" \
+  -out "$SSH_FILE" -d
+# Enable SSH authentication
+chmod 600 "$SSH_FILE" \
+  && printf "%s\n" \
+       "Host github.com" \
+       "  IdentityFile $SSH_FILE" \
+       "  LogLevel ERROR" >> ~/.ssh/config
