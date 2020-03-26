21d234121468cb9c292c0ddd42878a1a07fc1969
Jakob Braun <jakob.braun@posteo.de>
2020-03-26 14:52:01 +0100

    fixed travis

--- .ci/setup-ssh.sh
+++ .ci/setup-ssh.sh
@@ -2,11 +2,11 @@
 # setup-ssh.sh: load the SSH key 
 
 set -ev
-declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
+declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_0tz4)"
 # Decrypt the file containing the private key (put the real name of the variables)
 openssl aes-256-cbc \
-  -K $encrypted_4a328ad72b8a_key \
-  -iv $encrypted_4a328ad72b8a_iv \
+  -K $encrypted_63a949b43279_key \
+  -iv $encrypted_63a949b43279_iv \
   -in ".ci/travis_deploy_key.enc" \
   -out "$SSH_FILE" -d
 # Enable SSH authentication
