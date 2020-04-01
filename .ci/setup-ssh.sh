32d0f820cc5d8970db913f299fd92ca2f7b6d203
Jakob Braun <jakob.braun@posteo.de>
2020-04-01 12:47:48 +0200

    fixed travis

--- .ci/setup-ssh.sh
+++ .ci/setup-ssh.sh
@@ -3,7 +3,6 @@
 
 set -ev
 declare -r SSH_FILE="$(mktemp -u "$HOME"/.ssh/travis_temp_ssh_key_XXXX)"
-chmod 600 "$SSH_FILE"
 # Decrypt the file containing the private key (put the real name of the variables)
 # shellcheck disable=SC2154 # variables are set by travis
 openssl aes-256-cbc \
@@ -11,6 +10,7 @@ openssl aes-256-cbc \
   -iv "$encrypted_63a949b43279_iv" \
   -in ".ci/travis_deploy_key.enc" \
   -out "$SSH_FILE" -d
+chmod 600 "$SSH_FILE"
 # Enable SSH authentication
 printf "%s\n" \
        "Host github.com" \
