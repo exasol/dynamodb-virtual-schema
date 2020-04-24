0896f846230aa992d181317c02534e8a7eb50832
Jakob Braun <jakob.braun@posteo.de>
2020-04-24 14:27:17 +0200

    added javadoc

--- src/main/java/com/exasol/adapter/dynamodb/queryrunner/PlanDoesNotFitException.java
+++ src/main/java/com/exasol/adapter/dynamodb/queryrunner/PlanDoesNotFitException.java
@@ -1,5 +1,8 @@
 package com.exasol.adapter.dynamodb.queryrunner;
 
+/**
+ * This Exception is thrown by a QueryPlanFactory if the plan does not fit for the given query.
+ */
 public class PlanDoesNotFitException extends Exception {
     private static final long serialVersionUID = -3257463856415639740L;
 
