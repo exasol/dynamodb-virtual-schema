8bb7f3d5da7c52bac099d2ed804145ed4aa1fbc5
Jakob Braun <jakob.braun@posteo.de>
2020-05-01 15:24:11 +0200

    added tests

--- src/test/java/com/exasol/adapter/dynamodb/remotetablequery/LogicalOperatorTest.java
+++ src/test/java/com/exasol/adapter/dynamodb/remotetablequery/LogicalOperatorTest.java
@@ -1,8 +1,7 @@
 package com.exasol.adapter.dynamodb.remotetablequery;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 import java.util.List;
 
@@ -28,5 +27,33 @@ class LogicalOperatorTest {
         final PredicateTestVisitor visitor = new PredicateTestVisitor();
         TEST_PREDICATE.accept(visitor);
         assertThat(visitor.getVisited(), equalTo(PredicateTestVisitor.Visited.BINARY_LOGICAL_OPERATOR));
+    }
+
+    @Test
+    void testNotEqual() {
+        final LogicalOperator<Object> otherPredicate = new LogicalOperator<>(List.of(NO_PREDICATE),
+                LogicalOperator.Operator.OR);
+        assertThat(TEST_PREDICATE, not(equalTo(otherPredicate)));
+    }
+
+    @Test
+    void testHashNotEqual() {
+        final LogicalOperator<Object> otherPredicate = new LogicalOperator<>(List.of(NO_PREDICATE),
+                LogicalOperator.Operator.OR);
+        assertThat(TEST_PREDICATE.hashCode(), not(equalTo(otherPredicate.hashCode())));
+    }
+
+    @Test
+    void testEqual() {
+        final LogicalOperator<Object> otherPredicate = new LogicalOperator<>(List.of(NO_PREDICATE),
+                LogicalOperator.Operator.OR);
+        assertThat(TEST_PREDICATE, equalTo(TEST_PREDICATE));
+    }
+
+    @Test
+    void testHashCodeEqual() {
+        final LogicalOperator<Object> otherPredicate = new LogicalOperator<>(List.of(NO_PREDICATE),
+                LogicalOperator.Operator.OR);
+        assertThat(TEST_PREDICATE.hashCode(), equalTo(TEST_PREDICATE.hashCode()));
     }
 }
