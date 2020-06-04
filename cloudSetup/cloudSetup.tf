74fab13062270f64c1d4867111d583ce785b5575
Jakob Braun <jakob.braun@posteo.de>
2020-06-04 17:02:35 +0200

    added exasol and s3 to coludSetup

--- cloudSetup/cloudSetup.tf
+++ cloudSetup/cloudSetup.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "eu-central-1"
-  profile = "jakob"
+  profile = "default"
 }
 
 resource "aws_dynamodb_table" "open_library_test" {
@@ -22,4 +22,48 @@ resource "aws_dynamodb_table" "open_library_test" {
     name = "key"
     type = "S"
   }
+}
+
+resource "aws_s3_bucket" "s3-bucket" {
+  bucket = "dynamodb-virtual-schema-test-bucket"
+  acl    = "private"
+
+  tags = {
+    "exa:owner": "jakob.braun@exasol.com",
+    "exa:deputy": "Sebastian.Baer@exasol.com"
+    "exa:project": "DDBVS"
+    "exa:project.name": "Virtual Schema for DynamoDB"
+    "exa:stage": "development"
+    "Name": "S3 bucket for storing test data for testing Dynamodb Virtual Schema"
+  }
+}
+
+
+module "exasol" {
+  #source = "exasol/exasol/aws"
+  #version = "0.0.3"
+  source = "../../terraform-aws-exasol"
+
+  cluster_name = "dynamodb-vs-test-cluster"
+  database_name = "exadb"
+  ami_image_name = "Exasol-R6.2.6-BYOL"
+  sys_user_password = "eXaSol1337DB"
+  admin_user_password = "eXaSol1337OP"
+  management_server_instance_type = "m5.xlarge"
+  datanode_instance_type = "m5.2xlarge"
+  datanode_count = "2"
+  standbynode_count = "0"
+  public_ip = true
+
+  # These values can be obtained from other modules.
+  key_pair_name = "exasol-key-pair"
+  subnet_id = "subnet-ed85b690"
+  security_group_id = "sg-07599522f13906845"
+
+  # Variables used in tags.
+  project = "DDBVS"
+  project_name = "Virtual Schema for DynamoDB"
+  owner = "jakob.braun@exasol.com"
+  environment = "dev"
+  license = "./exasolution.lic"
 }
