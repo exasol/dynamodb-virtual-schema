730852d6447ba4fddecc6d1dd5955bba7b7ed80d
Jakob Braun <jakob.braun@posteo.de>
2020-09-07 08:43:14 +0200

    changes from review

--- src/test/java/com/exasol/adapter/document/DynamodbVsExasolTestDatabaseBuilder.java
+++ src/test/java/com/exasol/adapter/document/DynamodbVsExasolTestDatabaseBuilder.java
@@ -28,15 +28,14 @@ public class DynamodbVsExasolTestDatabaseBuilder extends ExasolTestDatabaseBuild
     }
 
     public void createUdf() throws SQLException {// TODO make DynamoDB independent
-        final StringBuilder statementBuilder = new StringBuilder("CREATE OR REPLACE JAVA SET SCRIPT ")
-                .append(ADAPTER_SCHEMA).append(".").append(UdfRequestDispatcher.UDF_PREFIX + "DYNAMO_DB").append("(")
-                .append(AbstractDataLoaderUdf.PARAMETER_DOCUMENT_FETCHER).append(" VARCHAR(2000000), ")
-                .append(AbstractDataLoaderUdf.PARAMETER_REMOTE_TABLE_QUERY).append(" VARCHAR(2000000), ")
-                .append(AbstractDataLoaderUdf.PARAMETER_CONNECTION_NAME).append(" VARCHAR(500)) EMITS(...) AS\n");
+        final StringBuilder statementBuilder = new StringBuilder(
+                "CREATE OR REPLACE JAVA SET SCRIPT " + ADAPTER_SCHEMA + "." + UdfRequestDispatcher.UDF_PREFIX
+                        + "DYNAMO_DB" + "(" + AbstractDataLoaderUdf.PARAMETER_DOCUMENT_FETCHER + " VARCHAR(2000000), "
+                        + AbstractDataLoaderUdf.PARAMETER_REMOTE_TABLE_QUERY + " VARCHAR(2000000), "
+                        + AbstractDataLoaderUdf.PARAMETER_CONNECTION_NAME + " VARCHAR(500)) EMITS(...) AS\n");
         addDebuggerOptions(statementBuilder, true);
-        statementBuilder.append("    %scriptclass ").append(UdfRequestDispatcher.class.getName()).append(";\n");
-        statementBuilder.append("    %jar /buckets/bfsdefault/default/").append(VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION)
-                .append(";\n");
+        statementBuilder.append("    %scriptclass " + UdfRequestDispatcher.class.getName() + ";\n");
+        statementBuilder.append("    %jar /buckets/bfsdefault/default/" + VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION + ";\n");
         statementBuilder.append("/");
         final String sql = statementBuilder.toString();
         LOGGER.info(sql);
