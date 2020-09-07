6c1a46305902408105d9e39fe493bd057bce8128
Jakob Braun <jakob.braun@posteo.de>
2020-09-07 11:12:21 +0200

    changes from review

--- src/test/java/com/exasol/adapter/document/DynamodbVsExasolTestDatabaseBuilder.java
+++ src/test/java/com/exasol/adapter/document/DynamodbVsExasolTestDatabaseBuilder.java
@@ -1,5 +1,8 @@
 package com.exasol.adapter.document;
 
+import static com.exasol.adapter.document.AbstractDataLoaderUdf.*;
+import static com.exasol.adapter.document.UdfRequestDispatcher.UDF_PREFIX;
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.sql.SQLException;
@@ -29,10 +32,9 @@ public class DynamodbVsExasolTestDatabaseBuilder extends ExasolTestDatabaseBuild
 
     public void createUdf() throws SQLException {// TODO make DynamoDB independent
         final StringBuilder statementBuilder = new StringBuilder(
-                "CREATE OR REPLACE JAVA SET SCRIPT " + ADAPTER_SCHEMA + "." + UdfRequestDispatcher.UDF_PREFIX
-                        + "DYNAMO_DB" + "(" + AbstractDataLoaderUdf.PARAMETER_DOCUMENT_FETCHER + " VARCHAR(2000000), "
-                        + AbstractDataLoaderUdf.PARAMETER_REMOTE_TABLE_QUERY + " VARCHAR(2000000), "
-                        + AbstractDataLoaderUdf.PARAMETER_CONNECTION_NAME + " VARCHAR(500)) EMITS(...) AS\n");
+                "CREATE OR REPLACE JAVA SET SCRIPT " + ADAPTER_SCHEMA + "." + UDF_PREFIX + "DYNAMO_DB" + "("
+                        + PARAMETER_DOCUMENT_FETCHER + " VARCHAR(2000000), " + PARAMETER_REMOTE_TABLE_QUERY
+                        + " VARCHAR(2000000), " + PARAMETER_CONNECTION_NAME + " VARCHAR(500)) EMITS(...) AS\n");
         addDebuggerOptions(statementBuilder, true);
         statementBuilder.append("    %scriptclass " + UdfRequestDispatcher.class.getName() + ";\n");
         statementBuilder.append("    %jar /buckets/bfsdefault/default/" + VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION + ";\n");
